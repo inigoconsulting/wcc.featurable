0202d830c22db2169f766fe3a399389bfa1e3861
Izhar Firdaus <izhar@inigo-tech.com>
2013-02-06 15:53:36 +0800

    added featureimage listing view (re #166)

---
+++ wcc/featurable/browser/featureimagelisting_view.py
@@ -0,0 +1,40 @@
+from five import grok
+from Products.ATContentTypes.interfaces.folder import IATFolder
+from Products.ATContentTypes.interfaces.topic import IATTopic
+from wcc.featurable.interfaces import IFeaturableSettings, IFeaturable
+from zope.component import getMultiAdapter, getUtility
+from plone.registry.interfaces import IRegistry
+
+grok.templatedir('templates')
+
+class FeatureImageListingView(grok.View):
+    grok.baseclass()
+    grok.name('featureimagelisting_view')
+    grok.template('featureimagelisting_view')
+
+    def get_featureimage_width(self):
+        registry = getUtility(IRegistry)
+        proxy = registry.forInterface(IFeaturableSettings)
+        return proxy.feature_image_width
+
+    def get_feature_image(self, obj):
+
+        registry = getUtility(IRegistry)
+        proxy = registry.forInterface(IFeaturableSettings)
+
+        placeholder = '<div style="height:%spx;width:%spx;"></div>' % (
+            proxy.feature_image_height,
+            proxy.feature_image_width,
+        )
+        if not IFeaturable.providedBy(obj):
+            return placeholder
+        scales = obj.restrictedTraverse('@@images')
+        result = scales.scale('feature_image', width=proxy.feature_image_width,
+                height=proxy.feature_image_height)
+        return result if result else placeholder
+
+class FolderFeatureImageListingView(FeatureImageListingView):
+    grok.context(IATFolder)
+
+class TopicFeatureImageListingView(FeatureImageListingView):
+    grok.context(IATTopic)
